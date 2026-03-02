853785c96c5c334a288d518927dac57ac5f286e1
NRanjie <112406691+NRanjie@users.noreply.github.com>
2026-03-02 15:51:34 +0100

    Update tile.cs

--- tile.cs
+++ tile.cs
@@ -2,17 +2,20 @@
 
 
 class Tile
-
 {
-  public string tileName;
-  public char tileSymbol;
-  public bool tileWalkable;
+    public string _tileName { get; }
+    public char _tileSymbol { get; }
+    public bool _tileWalkable { get; }
 
-  public Tile(string name, char symbol, bool walkable)
-  {
-    tileName = name;
-    tileSymbol = symbol;
-    tileWalkable = walkable;
+    public Tile(string name, char symbol, bool walkable)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Symbol = symbol;
+        Walkable = walkable;
+    }
+
+    public override string ToString() => $"{Name} ({Symbol}) Walkable={Walkable}";
+
   }
 
 } 
